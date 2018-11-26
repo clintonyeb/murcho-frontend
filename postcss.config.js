dc92eeaf8e9c713b227cb9c7f60e17d2091e47cd
Clinton Yeboah <clintonyeb@gmail.com>
2018-11-26 12:13:51 +0530

    added config for purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,18 @@
 const tailwindcss = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    purgecss({
+      content: [
+        './public/index.html',
+        './src/**/*.vue',
+        './src/**/*.js',
+        './src/*.vue',
+        './src/*.js'
+      ]
+    })
   ]
 }
